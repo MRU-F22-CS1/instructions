58947728f4b39919b55e9325d7b005fe9dc81b96
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-09-19 16:10:13 -0600

    Adding intro to Python tutorial

---
+++ tutorials/intro_to_python/fix_me.py
@@ -0,0 +1,10 @@
+name = "your name"
+age = "26"
+
+print("My name is", name, "and I am", age, "years old.")
+print("That's", age * 12, "months!")
+
+# Uncomment the code below when instructed.
+# program = "BCIS"
+# year = 1
+# print("I am in year", year, "of", program
